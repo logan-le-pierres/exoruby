cd458842f31702254145ceec3dc0aabec07440cc
logan <loganlepierres1992@gmail.com>
2020-04-07 17:23:44 +0100

    Exo 07 ; B

--- exo_07_b.rb
+++ exo_07_b.rb
@@ -1,6 +1,5 @@
 puts "Bonjour, c'est quoi ton blase ?"
-user_name = gets.chomp 
-puts user_name 
-
-# la methode gets.chomp permet de récupérer l'info demandée a l'utilisateur et la stock dans la variable "user_name". L'information récupérée est un string
-
+print "> "
+user_name = gets.chomp
+puts user_name
+# "puts" entraine un renvoi à la ligne alors que "print" maintient sur la meme ligne 
